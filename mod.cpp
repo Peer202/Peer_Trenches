cf0f326c00617fb4acaa10ada7a3d224dbd22acc
Peer <peer200@gmx.de>
2024-05-07 16:29:45 +0200

    change: Mod Information ingame

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,8 @@
-name = "Peer Trenches";
-dir = "@Peer_Trenches";
-author = "[W] Peer";
-logo = "logo_ca.paa";
+name            = "Chameleon Trenches";
+dir             = "@Chameleon_Trenches";
+author          = "[W] Peer";
+logo            = "logo_ca.paa";
+actionName		= "GitHub";
+action			= "https://github.com/Peer202/Peer_Trenches";			// Website URL, that can be accessed from the expansions me
+tooltip         = "Chameleon Trenches";
+tooltipOwned    = "Chameleon Trenches Owned";
