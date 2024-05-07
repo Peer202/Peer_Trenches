be996df371a4cb81fae3f4b82aaa65685bddd543
Peer <peer200@gmx.de>
2024-05-07 17:12:24 +0200

    Increase Version

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
-#define MAJOR 0
-#define MINOR 6
-#define PATCH 5 // `#define PATCHLVL` can also be used
+#define MAJOR 1
+#define MINOR 0
+#define PATCH 0 // `#define PATCHLVL` can also be used
 #define BUILD 0 // Optional
