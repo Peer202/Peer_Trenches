c0d76f1d332ff4d844ce75e4242e065f837400bf
Peer <peer200@gmx.de>
2024-05-12 14:43:52 +0200

    Cleanup Dependencies

--- addons/main/cfgPatches.hpp
+++ addons/main/cfgPatches.hpp
@@ -3,9 +3,18 @@ class CfgPatches {
         name = "Peer Trenches - Main";
         author = "[W] Peer";
         url = "Gruppe-W.de";
-        units[] = {};
+        units[] = {
+            "Peer_Trench_Straight_Long_Chameleon",
+            "Peer_Trench_Straight_Short_Chameleon",
+            "Peer_Trench_Straight_vsmall_Chameleon",
+            "Peer_Trench_Bent_Chameleon",
+            "Peer_Trench_Floor_Chameleon",
+            "Peer_Trench_Top_Chameleon",
+            "Peer_Trench_Top_Bunker_Chameleon",
+            "Peer_Trench_Top_small_Chameleon"
+            };
         weapons[] = {};
-        requiredAddons[] = {"A3_Structures_F_Exp","cba_main","ace_trenches"};
+        requiredAddons[] = {"A3_Structures_F_Exp"};
         requiredVersion = 0.1;
     };
 };
